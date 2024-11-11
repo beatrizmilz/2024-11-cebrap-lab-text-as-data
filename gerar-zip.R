176bc8057745ec7c64999c3bf447516a40ec07b9
Beatriz Milz <milz.bea@gmail.com>
2024-11-11 18:25:43 -0300

    gerando zip

--- gerar-zip.R
+++ gerar-zip.R
@@ -1,8 +1,8 @@
 # gerar_zip
 zip::zip(
-  zipfile = "2023-11-cebrap-lab-text-as-data.zip",
+  zipfile = "2024-11-cebrap-lab-text-as-data.zip",
   files = c(
-    "2023-11-cebrap-lab-text-as-data.Rproj",
+    "2024-11-cebrap-lab-text-as-data.Rproj",
     "referencias/1859-Texto do artigo-3971-2-10-20210608.pdf",
     list.files("scripts", full.names = TRUE),
     list.files("exercicios", full.names = TRUE),
