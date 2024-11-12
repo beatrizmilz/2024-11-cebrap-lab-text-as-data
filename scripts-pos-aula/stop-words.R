67440758b27175de36b8ff79182cb3a7a8cd0bbf
Beatriz Milz <milz.bea@gmail.com>
2024-11-12 07:48:20 -0300

    adiciona stop words

---
+++ scripts-pos-aula/stop-words.R
@@ -0,0 +1,37 @@
+# Sources que tem pt: "snowball", "stopwords-iso", "nltk"
+
+library(stopwords)
+# Vamos usar o snowball
+snowball <-
+  stopwords::stopwords(source = "snowball", language = "pt")
+
+# também dá para buscar no pacote tm
+stopwords_tm <- tm::stopwords("pt")
+
+# Vamos adicionar algumas palavras que não estão no snowball
+# Mas que olhando os resultados, aparecem muito
+# e não são relevantes para a análise
+extra_stop_words <- c(
+  "é",
+  "ser",
+  "pra",
+  "vai",
+  "portanto",
+  "quase",
+  "pois",
+  "algo",
+  "assim",
+  "ai",
+  "ainda",
+  "algum",
+  "sendo",
+  "existe",
+  "disso",
+  "disso",
+  "á", 
+  "ter",
+  "pode",
+  "sobre"
+)
+
+stop_words_completo <- unique(c(snowball, stopwords_tm, extra_stop_words))
